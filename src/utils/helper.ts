65346a4c73b220620408849d9c121bf6917e62ba
Lamiaa Gabriel <lamiaamonaem6@gmail.com>
2023-06-02 15:17:23 +0300

    revalidate db connection

--- src/utils/helper.ts
+++ src/utils/helper.ts
@@ -31,7 +31,7 @@ export const fetcher = async (
   url: RequestInfo | URL,
   options: RequestInit | undefined
 ) => {
-  return await fetch(`${process.env.NEXTAUTH_URL}/api${url}`, options).then(
+  return await fetch(`${process.env.VERCEL_URL}/api${url}`, options).then(
     (res) => res.json()
   )
 }
